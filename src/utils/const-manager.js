c9cf03e54444db31da1e5ac43de5710cf09e4d71
zhangziteng <751535279@qq.com>
2018-10-11 16:23:44 +0800

    考生登录系统 前台Web端页面框架--张子腾 2018年10月11日16:22:01

--- src/utils/const-manager.js
+++ src/utils/const-manager.js
@@ -33,7 +33,8 @@ const POP_TIP = {
     fileFormatFail: '您上传的文件格式不正确！',//by qitian
     completeInfo: '请完善信息',//by qitian
     checkInfo: '请核对信息',//,by qitian
-    confirm: '您确定执行此操作吗？'
+    confirm: '您确定执行此操作吗？',
+    choiceOne: '请选择一条选项'
 };
 
 /**
@@ -77,5 +78,57 @@ const REGEX = {
  * @Author qitian
  */
 const AJAX_URL = {
-    login: requestJson ? '../../jsonDatas/login.json' : ''
+    login: requestJson ? '../../jsonDatas/login.json' : '',
+    personDataTab: requestJson ? '../../jsonDatas/personDataTab.json' : '',
+    personProblem: requestJson ? '../../jsonDatas/personProblem.json' : '',
+    loginLog: requestJson ? '../../jsonDatas/loginLog.json' : '',
+    knowledgeData: requestJson ? '../../jsonDatas/knowledgeData.json' : '',
+    /**
+     *@desc 问题反馈数据
+     *@date 2018/09/27 09:22:00
+     *@author zhangziteng
+     */
+    reportProblem: requestJson ? '../../jsonDatas/reportProblem.json' : '',
+    allProblemData: requestJson ? '../../jsonDatas/allProblemData.json' : '',
+    checkAllProblemData: requestJson ? '../../jsonDatas/checkAllProblemData.json' : '',
+    /**
+     *@desc 招生计划数据
+     *@date 2018/10/10 10:29:51
+     *@author zhangziteng
+     */
+    recruitPlanData: requestJson ? '../../jsonDatas/recruitPlanData.json' : '',
+    //经销商管理 刘志杰 2018-09-25
+    distributorData: requestJson ? '../../jsonDatas/distributorData.json' : '',
+    //系统设置-答疑设置（预警列表） 刘志杰 2018-09-28
+    answerSetting: requestJson ? '../../jsonDatas/answerSetting.json' : '',
+    //用户管理 刘志杰 2018-09-28
+    userManage: requestJson ? '../../jsonDatas/userData.json' : '',
+    /**
+     *@desc 日志管理，答疑管理数据
+     *@date 2018/09/27 09:23:06
+     *@author 宣文斌
+     */
+    questionManage: requestJson ? '../../jsonDatas/questionManageData.json' : '',
+    SBOMChangeLogData: requestJson ? '../../jsonDatas/SBOMChangeLogData.json' : '',
+    operationLogData: requestJson ? '../../jsonDatas/operationLogData.json' : '',
+    announceManagementData: requestJson ? '../../jsonDatas/announceManagementData.json' : '',
+    /**
+     *@desc
+     *@date 2018/09/27 10:05:20
+     *@author 刘笑天
+     */
+    personnelConfiguration: requestJson ? '../../jsonDatas/personnelConfiguration.json' : '',
+    bomChange: requestJson ? '../../jsonDatas/bomChange.json' : '',
+    administratorList: requestJson ? '../../jsonDatas/administratorList.json' : '',
+    //崔雨鑫
+    announceData: requestJson ? '../../jsonDatas/announceData.json' : '',
+    announceDetailData: requestJson ? '../../jsonDatas/announceDetailData.json' : '',
+
+    //王克龙
+    knowledgeDetial: requestJson ? '../../jsonDatas/knowledgeDetial.json' : '',
+    IndexEvent: requestJson ? '../../jsonDatas/index-event.json' : '',
+    IndexData: requestJson ? '../../jsonDatas/index-data.json' : '',
+    IndexSpare: requestJson ? '../../jsonDatas/index-spare.json' : '',
+    vinQuery: requestJson ? '../../jsonDatas/vin-query.json' : '',
+    vinList: requestJson ? '../../jsonDatas/vin-list.json' : '',
 }
