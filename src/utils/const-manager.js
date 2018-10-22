08084347f19a5123d36524b22e13865519535ea3
Cangsfckcpu <Cangs_fckcpu@163.com>
2018-10-22 10:05:20 +0800

    刘笑天20181019

--- src/utils/const-manager.js
+++ src/utils/const-manager.js
@@ -129,6 +129,12 @@ const AJAX_URL = {
     examineeLogin: requestJson ? '../../jsonDatas/login.json' : requestUrl + 'api/generate/userinformation/userLogin',
     //准考证号验证 刘笑天 2018-10-19
     checkExaminationNumber: requestJson ? '../../jsonDatas/checkExaminationNumber.json' : requestUrl + '/api/generate/examineeinformation/checkExaminationNumber',
+    //报考分析 刘笑天 2018-10-22
+    signAnalysis: requestJson ? '../../jsonDatas/signAnalysis.json' : '',
+
+
+
+
     //崔雨鑫
     announceData: requestJson ? '../../jsonDatas/announceData.json' : '',
     announceDetailData: requestJson ? '../../jsonDatas/announceDetailData.json' : '',
